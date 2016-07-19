99bc16f4b794d59c73a061bd63659ba8ffec149a
javenkn <javenkn7@yahoo.com>
2016-07-18 23:00:09 -1000

    ran a query to get all of the make_titles with the make_code 'LAM'

--- indexing.sql
+++ indexing.sql
@@ -10,8 +10,11 @@ CREATE USER indexed_cars_user;
 
 \c indexed_cars
 
+-- Run the provided scripts/car_models.sql script on the indexed_cars database
 \i scripts/car_models.sql;
 
+-- Run the provided scripts/car_model_data.sql script on the indexed_cars database 10 times
+-- there should be 223380 rows in car_models
 \i scripts/car_model_data.sql;
 \i scripts/car_model_data.sql;
 \i scripts/car_model_data.sql;
@@ -24,3 +27,11 @@ CREATE USER indexed_cars_user;
 \i scripts/car_model_data.sql;
 
 SELECT COUNT(*) FROM car_models;
+
+\timing
+
+-- Run a query to get a list of all make_title values from the car_models table
+-- where the make_code is 'LAM', without any duplicate rows, and note the time somewhere. (should have 1 result)
+SELECT make_title FROM car_models WHERE make_code = 'LAM'
+  GROUP BY make_title;
+
