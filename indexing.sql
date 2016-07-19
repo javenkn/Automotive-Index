2179fa506519fa39f7c45a95acdea88bb06a7821
javenkn <javenkn7@yahoo.com>
2016-07-18 23:14:53 -1000

    finished timing select statements

--- indexing.sql
+++ indexing.sql
@@ -32,6 +32,28 @@ SELECT COUNT(*) FROM car_models;
 
 -- Run a query to get a list of all make_title values from the car_models table
 -- where the make_code is 'LAM', without any duplicate rows, and note the time somewhere. (should have 1 result)
-SELECT make_title FROM car_models WHERE make_code = 'LAM'
+SELECT make_title FROM car_models
+  WHERE make_code = 'LAM'
   GROUP BY make_title;
 
+-- Run a query to list all model_title values where the make_code is 'NISSAN', and the model_code is 'GT-R' without any duplicate
+-- rows, and note the time somewhere. (should have 1 result)
+SELECT model_title FROM car_models
+  WHERE make_code = 'NISSAN'
+  AND model_code = 'GT-R'
+  GROUP BY model_title;
+
+-- Run a query to list all make_code, model_code, model_title, and year from car_models where the make_code is 'LAM', and note
+-- the time somewhere. (should have 1360 rows)
+SELECT * FROM car_models
+  WHERE make_code = 'LAM';
+
+-- Run a query to list all fields from all car_models in years between 2010 and 2015, and note the time somewhere (should have
+-- 78840 rows)
+SELECT * FROM car_models
+  WHERE year >= 2010
+  AND year <= 2015;
+
+-- Run a query to list all fields from all car_models in the year of 2010, and note the time somewhere (should have 13140 rows)
+SELECT * FROM car_models
+  WHERE year = 2010;
